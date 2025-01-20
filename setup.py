887cd0b4eabcd43b31d5887716127718811e32de
Pratibha Singh <singhpinki6060@gmail.com>
2025-01-20 23:21:28 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name= "Shipment",
+    version="0.0.1",
+    author = 'pratibha',
+    author_email='singhpinki6060@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+
+)
